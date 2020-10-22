2a98e592c0f7311a5b4d304375fa379393ba72d4
williamswarren <warrenwilliams1996@gmail.com>
2020-10-22 14:03:00 -0700

    Cleaned up scraping output and writing to CSV file

--- Python_Script.py
+++ Python_Script.py
@@ -7,13 +7,20 @@ from selenium.webdriver.common.keys import Keys
 import requests
 from bs4 import BeautifulSoup
 import time
+import csv
+import re 
 
 #setting up the path where I placed the chromedriver executable file 
 #you might have some permission erros with Mac OS, this link should fix it https://docwhat.org/upgrading-to-catalina
 PATH = '/Users/warrenwilliams/Desktop/LinkedIn/chromedriver'
 
-#creating a list to store all the information from each individual person
-people = []
+#creating a csv file to store the results
+csv_file = open("lead_gen.csv","w")
+
+csv_writer = csv.writer(csv_file)
+
+csv_writer.writerow(["Name","Summary/Title","Link"])
+
 
 ### This could be a login function
 driver = webdriver.Chrome(PATH)
@@ -75,27 +82,52 @@ link5.click()
 ### Navigating to the people tab to find people in the industry
 
 ### Start Scraping The Profiles
-
-time.sleep(3)
-
-source = driver.page_source
-
-soup = BeautifulSoup(source,"lxml")
-
-profile = soup.find(class_="search-result__wrapper")
-profile2 = profile.find("span",class_="name actor-name")
-profile3 = profile.find("p",class_="subline-level-1 t-14 t-black t-normal search-result__truncate")
-print(profile)
-print(profile2)
-print(profile3)
-
 #print(driver.current_url) gets the current URL of the page that you are on
 #print(driver.page_source) gets all the HTML from the current page
 
-test1 = profile2.text
-test2 = profile3.text 
+time.sleep(3)
+#put the current HTML page in a beautiful soup object
+source = driver.page_source
+soup = BeautifulSoup(source,"lxml")
 
-print(test1,test2)
+#finding the name, title and links for a profile
+profile0 = soup.find(class_="search-results-container")
+profile1 = profile0.find(class_="search-result__wrapper")
+profile2 = profile0.find("span",class_="name actor-name")
+profile3 = profile0.find("p",class_="subline-level-1 t-14 t-black t-normal search-result__truncate")
+
+#adding the links for the page to a list
+profile_links = []
+
+for link in soup.find_all(href=re.compile("/in/")):
+    profile_links.append(link.get("href"))
+
+#choosing the first profile link for the first person    
+profile4 = profile_links[0]
+
+
+print("This is a list of the profile links: ", profile_links)
+print()
+
+#grabbing only the text from the html and stripping out any whitespace
+test1 = profile2.text
+test2 = profile3.text.strip() 
+test3 = "https://www.linkedin.com" + profile4 
+
+#printing to the console the first persons profile information
+print("Name: ",test1)
+print()
+print("Title/Summary: ",test2)
+print()
+print("Link: ",test3)
 ### Start Scraping the Profiles
 
+### Writing to the CSV File
+csv_writer.writerow([test1,test2,test3])
+csv_file.close
+### Writing to the CSV File
+
+### Opening CSV File in Application
+
+### Opening CSV File in Application
 ###
